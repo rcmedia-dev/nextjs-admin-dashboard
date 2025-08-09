0d4f5a25d82599bc0174c48e1929438d58816422
rcmedia-dev <rcmidia.dev@gmail.com>
2025-08-09 17:06:03 +0100

    fixing errors

---
+++ app/dashboard/costumers/page.tsx
@@ -0,0 +1,5 @@
+export default function CostumersPage(){
+    return(
+        <p>Constumers PAge</p>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashBoardPage(){
+    return(
+        <div>This is the Dashboard Page</div>
+    )
+}
